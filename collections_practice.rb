a277257b08873d92ee08f7e8baabf2963a6897b1
Sharmina Akhtar <sakht318@gmail.com>
2017-11-13 09:51:13 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -27,7 +27,7 @@ def find_a(array)
   ["apple", "orange", "pear", "avis", "arlo",
   "ascot" ].collect do |array|
     array = []
-    if array.start_with?("a")
+    if array.starts_with?("a")
      return array
     end
   end
