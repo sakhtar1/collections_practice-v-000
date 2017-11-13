7c7c605bf2c58bae8a75b036763ac764de039df0
Sharmina Akhtar <sakht318@gmail.com>
2017-11-13 10:55:13 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -16,7 +16,7 @@ def reverse_array(array)
 end
 
 def swap_elements(array)
-  swap_elements(["blake", "ashley", "scott"],0,2)
+  array[1], array[2] = array[0], array[2], array[1]
 end
 
 def kesha_maker(array)
