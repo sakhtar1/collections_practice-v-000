6fd3a2f19dadc43f455f8da698087afad93f81d6
Sharmina Akhtar <sakht318@gmail.com>
2017-11-13 09:03:00 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,7 +7,7 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-    array[1], array[2] = array[0], array[2], array[1]
+    array[0], array[1] = array[1], array[0], array[2]
 
 end
 
